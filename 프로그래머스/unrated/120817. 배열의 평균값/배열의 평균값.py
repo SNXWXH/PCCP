e97badc23d70ad86acd5bfc883996fffb98cf457
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2023-11-04 22:33:32 +0900

    [level unrated] Title: 배열의 평균값, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.py
@@ -1,6 +1,2 @@
 def solution(numbers):
-    sum = 0
-    length = len(numbers)
-    for i in range(length):
-        sum += numbers[i]
-    return sum / length
+    return sum(numbers) / len(numbers)
